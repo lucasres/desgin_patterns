e8986477678ebab88ba9453acec671b3ef2aab79
mandala21 <lucasresone@gmail.com>
2019-03-18 14:04:27 -0300

    change name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='dpatterns',
-      version='0.1',
+setup(name='djpatterns',
+      version='0.1.1',
       description='A helper for desgin patterns in python',
       url='https://github.com/mandala21/django_repository',
       author='Lucas Resende',
       author_email='lucasresone@gmail.com',
       license='MIT',
-      packages=['dpatterns'],
+      packages=['djpatterns'],
       zip_safe=False)
